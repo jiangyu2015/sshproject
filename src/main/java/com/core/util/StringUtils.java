0329da06dfb10759d56b3058c927ee7bf41f3cff
jiangyukun <187_6810_5877@sina.com>
2015-12-04 14:55:49 +0800

    写个工具类，给你用用

---
+++ src/main/java/com/core/util/StringUtils.java
@@ -0,0 +1,10 @@
+package com.core.util;
+
+/**
+ * jiangyukun on 2015-12-04 14:53
+ */
+public class StringUtils {
+    public static boolean isEmpty(String str) {
+        return str != null && !str.equals("");
+    }
+}
